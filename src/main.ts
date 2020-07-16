21d32c2df9442eba4fe17785848b8c51ab2b1031
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2020-07-16 18:59:59 -0300

    First commit; Added env variables; New conection to DB and first entity

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const PORT = process.env.PORT || 8080;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+  Logger.log(`Server running http://localhost:${PORT}`);
+}
+bootstrap();
